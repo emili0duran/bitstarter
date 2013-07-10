2f9b635560be540dfbb0e28faed04964d6c01e5d
emili0duran <emilio.duran@eam.es>
2013-07-10 06:46:22 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buff;
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+  fs.readFileSync('index.html', funtion(err, data){
+	response.send(data);
+  }
 });
 
 var port = process.env.PORT || 5000;
